668f6ae93018267a13e11187af38dfcc6d156b47
Edith Dimboiu <edith@dimboiu.com>
2024-12-19 13:52:32 +0100

    create Heading component

---
+++ components/Heading.jsx
@@ -0,0 +1,9 @@
+const Heading = ({ title }) => {
+  return (
+    <section className="bg-white mb-3 px-2 py-2">
+      <h1 className="text-2xl font-bold text-gray-800">{title}</h1>
+    </section>
+  );
+};
+
+export default Heading;
