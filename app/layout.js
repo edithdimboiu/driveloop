99dd36b8ac83a21c0578e8afba4d9fb8c1ef0f3b
Edith Dimboiu <edith@dimboiu.com>
2024-12-19 13:23:35 +0100

    use Header and Footer component

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,6 @@
 import { Inter } from "next/font/google";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 import "@/assets/styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -12,9 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Header />
         <main className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
           {children}
         </main>
+        <Footer />
       </body>
     </html>
   );
