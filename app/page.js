ccdd74ca635dcf2032ba673acf06c320f0cc1094
Edith Dimboiu <edith@dimboiu.com>
2024-12-19 13:26:20 +0100

    use car card component on homepage

--- app/page.js
+++ app/page.js
@@ -1,8 +1,14 @@
+import CarCard from "@/components/CarCard";
+import cars from "@/data/cars.json";
 
 export default function Home() {
   return (
     <>
-    <h1>DriveLoop</h1>
+      {cars.length > 0 ? (
+        cars.map(car => <CarCard car={car} key={car.id}></CarCard>)
+      ) : (
+        <p>No cars available at the moment. Please try again later.</p>
+      )}
     </>
   );
 }
