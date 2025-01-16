c58012d934da3133c3fe2e0a5cc34638e50f2131
Edith Dimboiu <edith@dimboiu.com>
2025-01-16 11:56:54 +0100

    create Car schema

---
+++ models/Car.js
@@ -0,0 +1,35 @@
+import { Schema, model, models } from "mongoose";
+
+const CarSchema = new Schema(
+  {
+    user_id: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    manufacturer: { type: String, required: true },
+    model: { type: String, required: true },
+    year: { type: Number, required: true, max: new Date().getFullYear() },
+    description: {
+      type: String,
+      required: true,
+      minLength: 10,
+      maxLength: 1000,
+    },
+    engine_type: {
+      type: String,
+      required: true,
+      enum: ["Electric", "Gasoline", "Diesel", "Hybrid"],
+    },
+    location: { type: String, required: true },
+    address: { type: String, required: true },
+    features: { type: String, required: true },
+    availability: { type: String, required: true },
+    price_per_hour: { type: Number, required: true, min: 0 },
+    minimum_rental_duration: { type: Number, required: true, min: 1 },
+    maximum_rental_duration: { type: Number, required: true, min: 1, max: 72 },
+    image: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Car = models.Car || model("Car", CarSchema);
+export default Car;
