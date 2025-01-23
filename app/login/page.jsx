cdd533b96b6ecf00caa21ed5d6c1cb063304981f
Edith Dimboiu <edith@dimboiu.com>
2025-01-23 16:27:16 +0100

    update login page to use the create session service

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,12 +1,30 @@
+"use client";
+
 import Heading from "@/components/Heading";
 import InputField from "@/components/InputField";
 import AuthFormActions from "@/components/AuthFormActions";
+import createSession from "../services/createSession";
+import { useActionState, useEffect } from "react";
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
 
 const LoginPage = () => {
+  const [state, formAction] = useActionState(createSession, {});
+  const router = useRouter();
+
+  useEffect(() => {
+    if (state.error) {
+      toast.error(state.error);
+    }
+    if (state.success) {
+      toast.success("Logged in successfully!");
+      setInterval(() => router.push("/"), 2000);
+    }
+  }, [state]);
   return (
     <div className="flex items-center justify-center">
       <div className="bg-white shadow-lg rounded-lg p-6 w-full max-w-sm mt-20">
-        <form>
+        <form action={formAction}>
           <Heading extraClassName="text-center mb-6" text="Login" />
           <InputField id="username"></InputField>
           <InputField containerMargin="mb-6" id="password" type="password" />
