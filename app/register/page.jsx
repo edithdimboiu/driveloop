46f0e6d551f17a171bdc7f145f18eaca24a3ed12
Edith Dimboiu <edith@dimboiu.com>
2025-01-23 16:26:27 +0100

    update register page to use the create user service

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,12 +1,30 @@
+"use client";
+
 import Heading from "@/components/Heading";
 import InputField from "@/components/InputField";
 import AuthFormActions from "@/components/AuthFormActions";
+import { useActionState, useEffect } from "react";
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
+import createUser from "../services/createUser";
 
 const RegisterPage = () => {
+  const [state, formAction] = useActionState(createUser, {});
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (state.error) toast.error(state.error);
+    if (state.success) {
+      toast.success("You can now log in!");
+      router.push("/login");
+    }
+  }, [state]);
+
   return (
     <div className="flex items-center justify-center">
       <div className="bg-white shadow-lg rounded-lg p-6 w-full max-w-sm mt-20">
-        <form>
+        <form action={formAction}>
           <Heading extraClassName="text-center mb-6" text="Register" />
           <InputField id="name"></InputField>
           <InputField id="username"></InputField>
