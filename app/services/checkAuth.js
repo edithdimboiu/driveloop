66ca109c8da7b10631ec56881ee346cc3df88746
Edith Dimboiu <edith@dimboiu.com>
2025-01-24 11:57:21 +0100

    create checkAuth service

---
+++ app/services/checkAuth.js
@@ -0,0 +1,45 @@
+"use server";
+import { cookies } from "next/headers";
+import jwt from "jsonwebtoken";
+import User from "@/models/User";
+
+async function checkAuth() {
+  const sessionCookie = cookies().get("auth-token");
+
+  if (!sessionCookie) {
+    return {
+      isAuthenticated: false,
+    };
+  }
+
+  try {
+    const decodedToken = jwt.verify(
+      sessionCookie.value,
+      process.env.JWT_SECRET
+    );
+
+    const user = await User.findById(decodedToken.id);
+
+    if (!user) {
+      return {
+        isAuthenticated: false,
+      };
+    }
+
+    return {
+      isAuthenticated: true,
+      user: {
+        id: user._id.toString(),
+        name: user.name,
+        username: user.username,
+      },
+    };
+  } catch (error) {
+    console.error("Error during authentication:", error);
+    return {
+      isAuthenticated: false,
+    };
+  }
+}
+
+export default checkAuth;
