136b13234e8932895a1b61a1f9e63edeac8f3d54
Edith Dimboiu <edith@dimboiu.com>
2025-02-07 14:48:29 +0100

    create myCar card component

---
+++ components/MyCarCard.jsx
@@ -0,0 +1,29 @@
+"use client";
+
+import Link from "next/link";
+import { FaEye } from "react-icons/fa";
+import DeleteButton from "./DeleteButton";
+
+const MyCarCard = ({ car }) => {
+  const carName = `${car.manufacturer} ${car.model} ${car.year}`;
+  const carId = car._id ? car._id.toString() : null;
+  return (
+    <div className="bg-white shadow rounded-lg p-4 mt-4 flex flex-col sm:flex-row justify-between items-center">
+      <div className="flex flex-col">
+        <h4 className="text-lg font-semibold">{carName}</h4>
+      </div>
+      <div className="flex flex-col sm:flex-row w-full sm:w-auto sm:space-x-2 mt-2 sm:mt-0">
+        <Link
+          href={`/cars/${car._id}`}
+          className="bg-blue-500 text-white px-4 py-2 rounded mb-2 sm:mb-0 w-full sm:w-auto text-center hover:bg-blue-700"
+        >
+          <FaEye className="inline mr-1" /> View
+        </Link>
+
+        <DeleteButton carId={carId} />
+      </div>
+    </div>
+  );
+};
+
+export default MyCarCard;
