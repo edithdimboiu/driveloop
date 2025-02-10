26105ca5444cc67de8b27989cfd408330b8a59a1
Edith Dimboiu <edith@dimboiu.com>
2025-02-10 15:39:50 +0100

    update service to convert car id and user id to string

--- app/services/getCarById.js
+++ app/services/getCarById.js
@@ -7,7 +7,11 @@ export async function getCarById(id) {
   try {
     await connectDB();
     const car = await Car.findById(id).lean();
-    return car;
+    return {
+      ...car,
+      _id: car._id.toString(),
+      user_id: car.user_id.toString(),
+    };
   } catch (error) {
     console.log("Failed to get car.", error);
     throw new Error("Car not found.");
